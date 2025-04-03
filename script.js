2e121a7634223a2421c0fe5494030ef8c5d1b803
ShevchenkoOl <uzlabini@gmail.com>
2025-04-03 20:19:40 +0200

    added modal and form

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const hamburger = document.getElementById('hamburger');
-const nav = document.querySelector('nav ul');
+const nav = document.querySelector('.list');
 
 hamburger.addEventListener('click', () => {
-    nav.classList.toggle('active');
+    nav.classList.toggle('open');
+});
+
+
+const modal = document.getElementById("modal");
+const openModalBtn = document.getElementById("openModal");
+const closeModalBtn = document.querySelector(".close");
+
+openModalBtn.addEventListener("click", () => {
+    modal.classList.add("show");
+    document.body.classList.add("modal-open");
+});
+
+closeModalBtn.addEventListener("click", () => {
+    modal.classList.remove("show");
+    document.body.classList.remove("modal-open"); 
 });
