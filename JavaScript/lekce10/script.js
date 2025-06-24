542b5201e5e3a66df8c44e1fc20753b4effd52be
ShevchenkoOl <uzlabini@gmail.com>
2025-06-24 22:50:19 +0200

    updated lekce 10

--- JavaScript/lekce10/script.js
+++ JavaScript/lekce10/script.js
@@ -106,28 +106,55 @@ const section = document.querySelector("#section");
 // }
 
 
-const BASE_URL = `https://api.openweathermap.org/data/2.5/weather`;
-const input = document.querySelector("#cityInput")
-const city = "Prague";
-const API_KEY = "6625860d1ff9072a31a44a52133e59cb";
+// const BASE_URL = `https://api.openweathermap.org/data/2.5/weather`;
+// const input = document.querySelector("#cityInput")
+// const city = "Pr";
+// const API_KEY = "6625860d1ff9072a31a44a52133e59cb";
 
-fetch(`${BASE_URL}?q=${city}&appid=${API_KEY}&lang=cz`)
-    .then(res => {
-        if(!res.ok){
-            throw new Error(res.status)
-        }
-        return res.json()
-    })
-    .then( data => { console.log(data);
-        const div = document.createElement("div");
-        div.innerHTML = `
-        <h2>Pocasi ve ${data.name}, ${data.sys.country}</h2>
-        <p>${data.weather[0].description}</p>
-        <p>Rychlost vetru: ${data.wind.speed}</p>
-        <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="${data.weather[0].icon}">
-        `;
-        section.append(div);
-    }
-    )
+// fetch(`${BASE_URL}?q=${city}&appid=${API_KEY}&lang=cz`)
+//     .then(res => {
+//         if(!res.ok){
+//             throw new Error(res.status)
+//             alert("")
+//         }
+//         return res.json()
+//     })
+//     .then( data => { console.log(data);
+//         const div = document.createElement("div");
+//         div.innerHTML = `
+//         <h2>Pocasi ve ${data.name}, ${data.sys.country}</h2>
+//         <p>${data.weather[0].description}</p>
+//         <p>Rychlost vetru: ${data.wind.speed}</p>
+//         <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="${data.weather[0].icon}">
+//         `;
+//         section.append(div);
+//     }
+//     )
 
-    .catch(error => console.error(error))
+//     .catch(error => console.error(error))
+
+
+// fetch(`url`)
+//     .then(res => {
+//         if(!res.ok){
+//             throw new Error(res.status)
+//         }
+//         return res.json()
+//     })
+//     .then( data => console.log(data))
+
+//     .catch(error => console.error(error))
+
+
+
+
+// async function loadData(){
+//     try{
+//         const res = await fetch(`url`)
+//         if(!res.ok){
+//             throw new Error(res.status)
+//         }
+//         return res.json()
+//     }
+//     catch(err) {console.error("Doslo k chybe:", err)}
+// }
